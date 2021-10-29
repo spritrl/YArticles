8f63aede3759f1426d500e0a1c537618963b5401
Leo Cormoreche <leocormo@gmail.com>
2021-10-29 16:18:46 +0200

    Leo: Create the GET method for get_articles

---
+++ PHP/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Index du projet Yartile
+ *
+ * PHP Version 7
+ *
+ * @category  DevApi
+ * @author    ANOUCH Mouad
+ * @author    DUDUC Antoine
+ * @author    REALINI Christophe
+ * @author    CORMORECHE Leo
+ * @copyright 2021 Ynov
+ * @version   GIT: <0>
+ * @link      https://github.com/spritrl/YArticles
+ */
+
+ // On appelle notre fichier de fonction
+require 'includes/fct.inc.php';
+
+// Demarre une nouvelle session ou reprend un existante
+session_start();
+
+// On recupere le path
+$path = $_SERVER['REQUEST_URI'];
+
+switch($path){
+    case '/articles':
+        include 'controlers/article/c_articles.php';
+        get_articles('127.0.0.1:8000/articles');
+        break;
+}
