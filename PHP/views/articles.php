79a61b8dd2b93e4992f3dec805cf26b394c30817
Anouch Mouad <mdanouch@Anouchs-MacBook-Pro.local>
2021-11-04 10:06:28 +0100

    mouad: style css

--- PHP/views/articles.php
+++ PHP/views/articles.php
@@ -4,24 +4,33 @@ $data = callAPI('GET', '127.0.0.1:8000/articles', false);
 $json = json_decode($data, true);
 $articles = $json['articles'];
 $links = $json['_links'];
-print_r($links);
+// print_r($articles);
+echo "<div class='articles'>";
 foreach ($articles as $article) {
     echo "<h2>The articles :";
     echo $article['article_id'];
     echo "</h2>";
-    echo "<ul>";
-    foreach ($article as $key => $value) {
+    // foreach ($article as $key => $value) {
             ?>
             
-            <li><?php echo "{$key} : {$value}"; ?></span></li>
+            <!-- <li><?php echo "title : {$article['title']}"; ?></span></li> -->
+            <div class="card" style="width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo "{$article['title']}"; ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?php echo "{$article['slug']}"; ?></h6>
+                <p class="card-text"><?php echo "{$article['content']}"; ?></p>
+                <a href="#" class="card-link"><?php echo "{$article['author']}"; ?></a>
+                <a href="#" class="card-link"><?php echo "{$article['date']}"; ?></a>
+            </div>
+            </div>
         
         <?php
-    }
-    echo "</ul>";
+    // }
 }
+echo "</div>";
 ?>
 
-<div>
+<div class='links'>
     <a href=<?php echo $links['prev']; ?>>previous</a> 
     <a href=<?php echo $links['next']; ?>>next page</a> 
 </div>
